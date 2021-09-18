f7ab5c00eda13bb4ec77679353e75ac7aa1a7349
Jesper Wendel Devantier <jwd@defmacro.it>
2021-09-18 23:43:22 +0200

    RenderPageFn def

--- gadfly/api.py
+++ gadfly/api.py
@@ -1,6 +1,13 @@
 from gadfly.utils import file_sha256, cwd
 from gadfly.cli import colors, pp_exc, pp_err_details
+from gadfly import Config
+from typing import Dict, Any
 import subprocess
+
+
+class RenderPageFn:
+    def __call__(self, page: str, template: str, context: Dict[str, Any]) -> None:
+        ...
 
 
 def sh(cmd: str, echo=True, shell=True, check=False, desc=None, **kwargs) -> subprocess.CompletedProcess:
