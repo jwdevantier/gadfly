c279ad17b1c8ddeb8928d433cd2516b4a725dba3
Jesper Wendel Devantier <jwd@defmacro.it>
2022-01-19 19:17:00 +0100

    templating code

---
+++ gadfly/templating.py
@@ -0,0 +1,320 @@
+from enum import Enum
+from typing import List, Union, Dict, Any, Optional, Generator, Callable
+from re import (
+    compile as re_compile,
+    match as re_match,
+    split as re_split,
+    MULTILINE as RE_MULTILINE)
+
+# <%= EXPR %>   --- supports multiline, not sure how useful that is.
+
+# supports inline python, multiple lines
+# (! DOES NOT SUPPORT '%>' being present anywhere in the code block --- perhaps remove this stx for '%% py' )
+# <% STATEMENTS... %>
+
+# supports BLOCKS
+# WHITESPACE* '%%' WHITESPACE* '/'? BLOCK_IDENTIFIER (WHITESPACE+ (WHITESPACE|ANY_CHAR))
+# So '%% py' {"block": "py", args: ""}
+# and '%% foo something else -> {"block": "foo", "args": "something else"}
+rgx_template_tokens = re_compile(
+    r"(<%=[\s\S]*?\%\>|<%[\s\S]*?\%\>|^[ \t\r\f\v]*%%[ \t\r\f\v]*/?[a-zA-Z].*$\n?)",
+    RE_MULTILINE
+)
+
+# name: the block identifier
+# args: None  OR whatever's between the outermost (), so '()' => '' and (x, y) => 'x, y'
+rgx_block_open = r"^[ \t\r\f\v]*%%[ \t\r\f\v]*(?P<name>/?[a-zA-Z]\w*)(?:\((?P<args>.*)\))?$"
+
+
+class Tokenizer:
+    # list/slice of tokens
+    _tokens: List[str]
+    # current position of tokenizer in list of tokens
+    _pos: int
+
+    def __init__(self):
+        self._pos = 0
+
+    @staticmethod
+    def from_source(source: str) -> "Tokenizer":
+        t = Tokenizer()
+        t._tokens = re_split(rgx_template_tokens, source)
+        return t
+
+    def copy(self) -> "Tokenizer":
+        """Creates lightweight/shallow copy over token stream."""
+        t = Tokenizer()
+        t._tokens = self._tokens[self._pos:]
+        t._pos = self._pos
+        return t
+
+    # support copy.copy
+    __copy__ = copy
+
+    def peek(self) -> str:
+        """Get current token without advancing"""
+        return self._tokens[self._pos]
+
+    def next(self) -> str:
+        """Advance by one token"""
+        pos = self._pos
+        self._pos = pos + 1
+        return self._tokens[pos]
+
+    def has_more(self) -> bool:
+        """True iff. there are more tokens to parse"""
+        return self._pos < len(self._tokens)
+
+    def __str__(self):
+        return f"""Tokenizer<pos: {self._pos}, len: {len(self._tokens)}>"""
+
+
+class TokenType(Enum):
+    TEXT = 0
+    BLOCK = 1
+    PY_EXPR = 2
+    PY_BLOCK = 3
+
+    def __repr__(self):
+        return str(self.name)
+
+
+def cst_build(tokenizer: Tokenizer) -> list:
+    ast_stack = []
+    current = []
+    while tokenizer.has_more():
+        token = tokenizer.next()
+        if token.startswith("<%="):
+            current.append([TokenType.PY_EXPR, token])
+        elif token.startswith("<%"):
+            current.append([TokenType.PY_BLOCK, token])
+        elif token.startswith("%%"):
+            m = re_match(rgx_block_open, token)
+            if not m:
+                raise ValueError(f"syntax error in block tag: {repr(token)}")
+            name = m.group("name")
+            args = m.group("args")
+
+            if not name.startswith("/"):
+                # new block, push current block to stack
+                ast_stack.append(current)
+                current = [TokenType.BLOCK,
+                           [name, args.strip()] if (args and args.strip() != "") else [name],
+                           token]
+            else:
+                # end of block, but is it the right one?
+                if name[1:] != current[1][0]:
+                    raise ValueError(f"expected {repr(current[1][0])}, got {repr(name)}")
+                current.append(token) # append END (to maintain CST)
+                child = current
+                current = ast_stack.pop()
+                current.append(child)
+        else:
+            current.append([TokenType.TEXT, token])
+    if ast_stack:
+        raise ValueError(f"stack should be empty, is {repr(ast_stack)}")
+
+    return current
+
+
+def cst_flatten(cst: list) -> Generator[str, None, None]:
+    if len(cst) == 0:
+        return
+
+    if isinstance(cst[0], TokenType):
+        ntype = cst[0]
+        if ntype == TokenType.BLOCK:
+            yield cst[2]  # yield open line
+            yield from cst_flatten(cst[3:-1])  # yield from children
+            yield cst[-1]  # yield close line
+        elif ntype in (TokenType.TEXT, TokenType.PY_BLOCK, TokenType.PY_EXPR):
+            yield cst[1]
+        else:
+            raise ValueError(f"unknown token type {repr(ntype)}")
+    else:
+        # top-level (~PROGN) or body of some block
+        for elem in cst:
+            yield from cst_flatten(elem)
+
+
+class CodeBuilder:
+    code: List[Union[str, "CodeBuilder"]]
+    indent_level: int
+    indent_step: int
+
+    def __init__(self, indent_step=4):
+        self.code = []
+        if indent_step <= 0:
+            raise ValueError("indentation step must be a positive number")
+        self.indent_step = indent_step
+        self.__indent_level = 0
+
+    def __str__(self) -> str:
+        return "".join(str(c) for c in self.code)
+
+    @property
+    def indent_level(self) -> int:
+        return self.__indent_level
+
+    def add_line(self, line) -> None:
+        self.code.extend([
+            " " * self.indent_level,
+            line,
+            "\n"
+        ])
+
+    def add_section(self) -> "CodeBuilder":
+        section = CodeBuilder(indent_step=self.indent_step)
+        section.__indent_level = self.indent_level
+
+        self.code.append(section)
+        return section
+
+    def indent(self) -> None:
+        self.__indent_level += self.indent_step
+
+    def dedent(self) -> None:
+        new_indent = self.indent_level - self.indent_step
+        if new_indent < 0:
+            raise ValueError("cannot dedent - indentation level would be negative!")
+        self.__indent_level = new_indent
+
+    def eval(self) -> Dict[str, Any]:
+        """Evaluate code and return top-level definitions.
+
+        Returns:
+            Dictionary of top-level variable definitions.
+        """
+        # ensure top-level is being evaluated
+        assert self.indent_level == 0
+
+        py_src = str(self)
+        global_ns = {}
+        exec(py_src, global_ns)
+        return global_ns
+
+
+class TemplateSyntaxError(ValueError):
+    def __init__(self, msg: str, thing: Any):
+        self.msg = msg
+        self.thing = thing
+        super().__init__(f"{msg}: {repr(thing)}")
+
+
+CompileCSTFn = Callable[[list], None]
+BlockCompilerFn = Callable[[str, str, list, CodeBuilder, CompileCSTFn], None]
+
+
+def block_compile_py(name: str,
+                     args: str,
+                     body_cst: list,
+                     code: CodeBuilder,
+                     compile: CompileCSTFn):
+    body_txt = "".join(cst_flatten(body_cst))
+    code.add_line(
+        f"""exec({repr(body_txt)}, context)"""
+    )
+
+
+def block_compile_doc(name: str,
+                      args: str,
+                      body_cst: list,
+                      code: CodeBuilder,
+                      compile: CompileCSTFn):
+    # doc blocks are just for documenting the template, no processing needed
+    pass
+
+
+class Template:
+    def __init__(self,
+                 cst: list,
+                 *contexts: Dict[str, Any],
+                 compilers: Optional[Dict[str, BlockCompilerFn]] = None):
+        # construct context from successively merging each given context dict
+        self.context = {}
+        for context in contexts:
+            self.context.update(context)
+
+        self._code = CodeBuilder()
+        # use this to store/buffer output until required to flush it out (using `flush_output`)
+        # this reduces the amount of code generated in the resulting render function
+        self.__output_buffer = []
+        self.__block_compilers = compilers
+
+        code = self._code
+        code.add_line("def render_function(context):")
+        code.indent()
+        code.add_line("result = []")
+
+        # local aliases to reduce lookup overhead
+        code.add_line("append_result = result.append")
+        code.add_line("extend_result = result.extend")
+        code.add_line("to_str = str")
+
+        # TODO: this approach will lose the ability to insert a CodeBuilder
+        #       as a point of later extension UNLESS we alter things somehow
+        #       (but do we need it?)
+        if len(cst) == 0:
+            raise ValueError("program cannot be empty")
+
+        self.__compile_body(
+            cst if isinstance(cst[0], list) else [cst]
+        )
+        self.flush_output()
+        code.add_line("""return "".join(result)""")
+        code.dedent()
+        self._render_function = code.eval()["render_function"]
+
+    def __compile_body(self, body: list):
+        stack = []
+        current = body
+        for node in current:
+            ntype = node[0]
+            if isinstance(ntype, list):
+                stack.append(current)
+                current = node
+            elif ntype == TokenType.TEXT:
+                txt = node[1]
+                if txt:
+                    self.__output_buffer.append(repr(txt))
+            elif ntype == TokenType.PY_EXPR:
+                expr_code = node[1][3:-2].strip()
+                self.__output_buffer.append(
+                    f"""to_str(eval({repr(expr_code)}, context))"""
+                )
+            elif ntype == TokenType.PY_BLOCK:
+                self.flush_output()
+                code_block = node[1][2:-2].strip()
+                self._code.add_line(
+                    f"""exec({repr(code_block)}, context)"""
+                )
+            elif ntype == TokenType.BLOCK:
+                # BLOCK: [<TYPE> <HEADER> <RAW HEADER> BODY... <RAW END>]
+                # where HEADER: [<name>, <args>]
+                name = node[1][0]
+                args = node[1][1] if len(node[1]) == 2 else None
+                body = node[3:-1]
+
+                # locate appropriate compiler and pass control to it
+                block_compiler = self.__block_compilers.get(name)
+                if not block_compiler:
+                    raise ValueError(f"unknown block type {repr(name)}")
+
+                self.flush_output()
+                # insert placeholder which the block can use to generate its output
+                block_code_builder = self._code.add_section()
+                block_compiler(name, args, body, block_code_builder, self.__compile_body)
+            else:
+                raise TemplateSyntaxError("unexpected element in state __compile_node", node)
+
+    def flush_output(self):
+        """Force `buffered` to the code builder."""
+        if len(self.__output_buffer) == 1:
+            self._code.add_line(f"append_result({self.__output_buffer[0]})")
+        elif len(self.__output_buffer) > 1:
+            self._code.add_line(f"""extend_result([{", ".join(self.__output_buffer)}])""")
+        del self.__output_buffer[:]
+
+    def render(self, context: Optional[Dict[str, Any]] = None):
+        render_context = {**self.context, **(context or {})}
+        return self._render_function(render_context)
--- tests/test_templating.py
+++ tests/test_templating.py
@@ -1,9 +1,9 @@
-from gadfly.templating import tokenize, TTemplate
+from gadfly.templating import *
 import pytest
 
 
-def test_something_cool():
-    x = tokenize("""
+def test_tokenizer():
+    x = Tokenizer.from_source("""
 hello world
 <% code time %>
 
@@ -20,7 +20,7 @@ something else
 %% py
 %% /py
 """)
-    assert x == [
+    assert x._tokens == [
         "\nhello world\n",
         "<% code time %>",
         "\n\n",
@@ -28,14 +28,111 @@ something else
         "\n\nsomething else\n\n",
         "<%= foo() %>",
         "\n\n",
-        "%% py",
-        "\n",
-        "%% /py",
-        "\n"
+        "%% py\n",
+        "",
+        "%% /py\n",
+        ""
     ]
 
 
-def _eval_template(template: TTemplate, render_ctx: dict):
+@pytest.mark.parametrize("ast, tokens", [
+    ([], []),
+    ([TokenType.TEXT, "hello, world\n"],
+     ["hello, world\n"]),
+    ([TokenType.PY_EXPR, "<%= name %>"],
+     ["<%= name %>"]),
+    ([TokenType.PY_BLOCK, "<%\nprint(3.14)\nprint(foo)\n%>"],
+     ["<%\nprint(3.14)\nprint(foo)\n%>"]),
+    ([TokenType.BLOCK, ["py"], "%% py",
+      [TokenType.TEXT, "print(2 + x)"],
+      "%% /py"],
+     ["%% py", "print(2 + x)", "%% /py"]),
+    ([[TokenType.TEXT, "Hello, "],
+      [TokenType.PY_BLOCK, """<% print("Mr. ") %>"""],
+      [TokenType.PY_EXPR, "<%= name %>"],
+      [TokenType.TEXT, "!"]],
+     ["Hello, ", """<% print("Mr. ") %>""", "<%= name %>", "!"])
+])
+def test_ast_flatten(ast, tokens):
+    assert list(cst_flatten(ast)) == tokens
+
+
+def test_cst_roundtrip():
+    tokenizer = Tokenizer.from_source("""\
+hello world
+<% code time %>
+
+<%
+much
+more
+code
+%>
+
+something else
+
+<%= foo() %>
+
+%% py
+def add(x, y):
+    return x + y
+%% /py
+%% some_block(x, y)
+some block's content
+%% py
+print(3.1415)
+%% /py
+%% /some_block""")
+    toks = [t for t in tokenizer._tokens]
+    cst = cst_build(tokenizer)
+    assert toks == [t for t in cst_flatten(cst)]
+
+
+def test_cst_parser():
+    tokenizer = Tokenizer.from_source("""\
+hello world
+<% code time %>
+
+<%
+much
+more
+code
+%>
+
+something else
+
+<%= foo() %>
+
+%% py
+def add(x, y):
+    return x + y
+%% /py
+%% some_block(x, y)
+some block's content
+%% py
+print(3.1415)
+%% /py
+%% /some_block""")
+    assert cst_build(tokenizer) == [
+        [TokenType.TEXT, "hello world\n"],
+        [TokenType.PY_BLOCK, "<% code time %>"],
+        [TokenType.TEXT, "\n\n"],
+        [TokenType.PY_BLOCK, "<%\nmuch\nmore\ncode\n%>"],
+        [TokenType.TEXT, "\n\nsomething else\n\n"],
+        [TokenType.PY_EXPR, "<%= foo() %>"],
+        [TokenType.TEXT, "\n\n"],
+        [TokenType.BLOCK, ["py"], "%% py\n",
+         [TokenType.TEXT, "def add(x, y):\n    return x + y\n"], "%% /py\n"],
+        [TokenType.TEXT, ""],
+        [TokenType.BLOCK, ["some_block", "x, y"], "%% some_block(x, y)\n",
+         [TokenType.TEXT, "some block's content\n"],
+         [TokenType.BLOCK, ["py"], "%% py\n",
+          [TokenType.TEXT, "print(3.1415)\n"], "%% /py\n"],
+         [TokenType.TEXT, ""], "%% /some_block"],
+        [TokenType.TEXT, ""],
+    ]
+
+
+def _eval_template(template: Template, render_ctx: dict):
     source_code = str(template._code)
     eval_env = {}
     # adds 'render_function' to context, otherwise unchanged
@@ -45,33 +142,41 @@ def _eval_template(template: TTemplate, render_ctx: dict):
     return res
 
 
-def _dump_template_to_file(template: TTemplate, fpath) -> None:
+def _dump_template_to_file(template: Template, fpath) -> None:
     source_code = str(template._code)
     with open(fpath, "w") as fh:
         for line in source_code:
             fh.write(line)
 
 
-@pytest.mark.parametrize("expr, context, result", [
-    ("5 + 5", {}, "10"),
-    (""""hello" + "world" + '!'""", {}, "helloworld!"),
-    # test rendering using provided context
-    ("pi", {"pi": 3.1415}, "3.1415"),
+@pytest.mark.parametrize("src, ctx, out", [
+    ("hello, world!", {}, "hello, world!"),
+    ("hello, <%= name %>!", {"name": "joe"}, "hello, joe!"),
+    ("hello, <%= name %>!", {"name": "jane"}, "hello, jane!"),
+    # code block alone does not effect the output
+    ("<% 2 + 3 %>", {}, ""),
+    # code block can influence render context
+    ("<% x = 4 %>number <%= x %>!", {}, "number 4!"),
+    # code block can read the existing render context and modify it
+    ("<% x = x + 2 %>number <%= x %>!", {"x": 2}, "number 4!"),
 ])
-def test_code_expr(expr, context, result):
-    template = TTemplate(f"""<%= {expr} %>""")
-    assert _eval_template(template, context) == result
+def test_compiler_one_liners(src, ctx, out):
+    cst = cst_build(Tokenizer.from_source(src))
+    template = Template(cst, compilers={})
+    # _dump_template_to_file(template, "/tmp/code.py")
+    assert _eval_template(template, ctx) == out
 
 
-def test_code_exprs_and_code_blocks__no_context():
-    template = TTemplate("""\
-hello, I am <%= 2 + 3 %> years old!
-<%
-def add2(x, y):
+@pytest.mark.parametrize("ctx, out", [
+    ({"a": 1, "b": 5}, "Hello, world!\nSo, 1+5 -> 6")
+])
+def test_py_block_compiler(ctx, out):
+    cst = cst_build(Tokenizer.from_source("""\
+Hello, world!
+%% py
+def add(x, y):
     return x + y
-%>
-So, 3 + 2 is <%= add2(3, 2) %>!""")
-    assert _eval_template(template, {}) == """\
-hello, I am 5 years old!
-
-So, 3 + 2 is 5!"""
+%% /py
+So, <%= a %>+<%= b %> -> <%= add(a, b) %>"""))
+    template = Template(cst, compilers={"py": block_compile_py})
+    assert _eval_template(template, ctx) == out
